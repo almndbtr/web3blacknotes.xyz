fa431487cbb9b3863ac409a3fadc0124e2c6d0b3
almndbtr.eth <78528185+almndbtr@users.noreply.github.com>
2022-01-29 22:53:50 +0000

    fix: update head.title

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Index({ allPosts }) {
     <>
       <Layout>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>🎹 Web3 Black Notes</title>
         </Head>
         <Container>
           <Intro />
