60b3100ef919253cfd5905c5c11187c80cebc411
almndbtr.eth <78528185+almndbtr@users.noreply.github.com>
2022-01-29 22:53:15 +0000

    hotfix: update frontend presentation

--- components/footer.js
+++ components/footer.js
@@ -7,22 +7,8 @@ export default function Footer() {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
+            Co-created with ♥️ by <a href="https://twitter.com/jamesgardin_">JamesGardin.eth</a>, <a href="https://twitter.com/iampatjunior">patjunior.eth</a>, and <a href="https://twitter.com/almndbtr">almndbtr.eth</a>.
           </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a
-              href="https://nextjs.org/docs/basic-features/pages"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              Read Documentation
-            </a>
-            <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-              className="mx-3 font-bold hover:underline"
-            >
-              View on GitHub
-            </a>
-          </div>
         </div>
       </Container>
     </footer>
