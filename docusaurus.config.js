58e611c6bbf5d7af820ccc0447872af45797aaf6
almndbtr.eth <78528185+almndbtr@users.noreply.github.com>
2022-02-16 13:57:45 +0000

    feat: port to docusaurus

---
+++ docusaurus.config.js
@@ -0,0 +1,129 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: '🎹 Web3 Black Notes',
+  tagline: 'Creating culture and space with the community.',
+  url: 'https://www.web3blacknotes.xyz/',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'almndbtr',
+  projectName: 'web3blacknotes.xyz',
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/almndbtr/web3blacknotes.xyz/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/almndbtr/web3blacknotes.xyz/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Web3 Black Notes',
+        logo: {
+          alt: 'Web3 Black Notes Logo',
+          src: 'img/the-black-notes.jpeg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'About',
+          },
+          {
+            to: '/blog',
+            label: 'Show Notes',
+            position: 'left'
+          },
+          {
+            href: 'https://github.com/almndbtr/web3blacknotes.xyz',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          // {
+          //   title: 'Docs',
+          //   items: [
+          //     {
+          //       label: 'Tutorial',
+          //       to: '/docs/intro',
+          //     },
+          //   ],
+          // },
+          {
+            title: 'Twitter',
+            items: [
+              {
+                label: 'Kizzie 👩🏾‍🔬',
+                href: 'https://twitter.com/PsalmOne',
+              },
+              {
+                label: 'JamesGardin.eth. 🏆 #CityWideChamps 🎹 🧙‍♀️',
+                href: 'https://twitter.com/jamesgardin_',
+              },
+              {
+                label: 'patjunior.eth 🅴',
+                href: 'https://twitter.com/iampatjunior',
+              },
+              {
+                label: 'Almond 🎹',
+                href: 'https://twitter.com/almndbtr',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              // {
+              //   label: 'About',
+              //   to: '/docs/intro',
+              // },
+              // {
+              //   label: 'Show Notes',
+              //   to: '/blog',
+              // },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/almndbtr/web3blacknotes.xyz',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Web3 Black Notes. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
